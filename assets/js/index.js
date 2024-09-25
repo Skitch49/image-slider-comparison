82d03a10823990f4d0a2f3ccc92c90ef9bd25f2e
Skitch49 <delaunay.alexis49@gmail.com>
2024-09-25 18:22:16 +0200

    add loader

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,17 @@ containers.forEach((container) => {
   const range = container.querySelector(".disclosure-range");
 
   if (dragLine && backgroundAfter && range) {
-    
     range.addEventListener("input", function () {
       backgroundAfter.style.width = range.value + "%";
       dragLine.style.left = range.value + "%";
     });
   }
+});
+
+const loader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  loader.classList.add("loading-out");
+  setTimeout(() => {
+    loader.remove();
+  }, 200);
 });
